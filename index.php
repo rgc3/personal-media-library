64f442a96df3441f993e33d93ec40cfa4e9ac388
rgc <rgc3dev@gmail.com>
2016-06-27 14:01:51 -0400

    Include header for index and suggest pages

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-
-<html>
-<head>
-	<title><?php echo "Personal Media Library"; ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-<body>
-
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
-
-			<ul class="nav">
-                <li class="books"><a href="catalog.php?cat=books">Books</a></li>
-                <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
-                <li class="music"><a href="catalog.php?cat=music">Music</a></li>
-                <li class="suggest"><a href="suggest.php">Suggest</a></li>
-            </ul>
-
-		</div>
-
-	</div>
+<?php include("includes/header.php"); ?>
 
 	<div id="content">
 		<div class="section catalog random">
@@ -38,7 +15,7 @@
 
 		</div>
 
-	</div>
+	</div><!-- end content -->
 
 	<div class="footer">
 
---
+++ suggest.php
@@ -0,0 +1,5 @@
+<?php include("includes/header.php"); ?>
+
+<div class="section page">
+  <h1>Suggest a Media Item</h1>
+</div>
