664886394d63035b0d27b665de3eff2c029ec27b
rgc <rgc3dev@gmail.com>
2016-07-05 14:39:36 -0400

    Add include functions

--- catalog.php
+++ catalog.php
@@ -1,5 +1,6 @@
 <?php
 include("includes/data.php");
+include("includes/functions.php");
 
 $pageTitle = "Full Catalog";
 $section = null;
@@ -27,13 +28,11 @@ include("includes/header.php"); ?>
     <h1><?php echo $pageTitle; ?></h1>
 
     <ul class="items">
-      <?php foreach($catalog as $item) {
-        echo "<li><a href='#'><img src='"
-         . $item["img"] . "' alt='"
-         . $item["title"] . "' />"
-         . "<p>View Details</p>"
-         . "</a></li>";
-      } ?>
+      <?php
+      foreach($catalog as $id => $item) {
+        echo get_item_html($id,$item);
+      }
+      ?>
     </ul>
 
   </div>
