d785590a702ca2041a875ebace87cdb47be8432f
rgc <rgc3dev@gmail.com>
2016-07-07 15:56:21 -0400

    Create process file

--- process.php
+++ process.php
@@ -1,1 +1,15 @@
-Processing form data
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$details = $_POST["details"];
+
+echo "<pre>"
+$email_body = "";
+$email_body .= "Name " . $name . "\n";
+$email_body .= "Email " . $email . "\n";
+$email_body .= "Details " . $details . "\n";
+echo $email_body;
+echo "</pre>"
+
+//To Do: Send Email
+?>
