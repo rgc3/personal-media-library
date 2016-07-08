5a8e95f9b33da8c66a20c8fc8bcd0ed53d3ca410
rgc <rgc3dev@gmail.com>
2016-07-07 20:51:47 -0400

    Form validation

--- suggest.php
+++ suggest.php
@@ -1,18 +1,32 @@
 <?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$details = $_POST["details"];
 
-echo "<pre>";
-$email_body = "";
-$email_body .= "Name " . $name . "\n";
-$email_body .= "Email " . $email . "\n";
-$email_body .= "Details " . $details . "\n";
-echo $email_body;
-echo "</pre>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = trim(filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING));
+  $email = trim(filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL));
+  $details = trim(filter_input(INPUT_POST,"details",FILTER_SANITIZE_SPECIAL_CHARS));
 
-//To Do: Send Email
-header("location:thanks.php");
+  //Validation
+  if ($name == "" || $email == "" || $details == "") {
+    echo "Please fill in the required fields: Name, Email and Details";
+    exit;
+  }
+  //Honeypot Spam
+  if ($_POST["address"] != "") {
+    echo "Bad form input";
+    exit;
+  }
+
+  echo "<pre>";
+  $email_body = "";
+  $email_body .= "Name " . $name . "\n";
+  $email_body .= "Email " . $email . "\n";
+  $email_body .= "Details " . $details . "\n";
+  echo $email_body;
+  echo "</pre>";
+
+  //To Do: Send Email
+  header("location:suggest.php?status=thanks");
+}
 
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
@@ -22,6 +36,9 @@ include("includes/header.php"); ?>
 <div class="section page">
   <div class="wrapper">
     <h1>Suggest a Media Item</h1>
+    <?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
+      echo "<p>Thanks for your email! I&rsquo;ll check out your suggestion shortly!</p>"
+    } else { ?>
     <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
     <form method="post" action="suggest.php">
       <table>
@@ -37,9 +54,16 @@ include("includes/header.php"); ?>
           <th><label for="details">Suggest Item Details</label></th>
           <td><textarea name="details" id="details"></textarea></td>
         </tr>
+        //Honeypot Span Filter, users will not see field but robots will fill it in
+        <tr style="display:none">
+          <th><label for="address">Address</label></th>
+          <td><textarea name="address" id="address"></textarea></td>
+          <p>Please leave this field blank</p>
+        </tr>
       </table>
       <input type="submit" value="send" />
     </form>
+    <?php } ?>
   </div>
 
 </div>
